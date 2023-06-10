705fe97cf259fe2818865b22a94d76ae6b7af6ee
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2023-06-10 13:59:57 -0300

    se resuelven errores de alta de peliculas de altaPelicula.js y altaPelicula.html

--- altaPelicula.js
+++ altaPelicula.js
@@ -1,95 +1,64 @@
-/*  const arrayPersonajes = []
+/* Obtener elementos HTML */
+let nombrePelicula = document.getElementById('nombrePelicula')
+let genero = document.getElementById('genero')
+let desc = document.getElementById('desc')
+/* Lo que me falto en la clase fue agregar un div en html y obtenerlo por JS */
+let divPadre = document.getElementById('divPadre')
 
- const divPadre = document.getElementById('cardPelicula')
+let arrayPel = []
 
- const personajes = fetch('https://rickandmortyapi.com/api/character')
- .then(repuestaApi => repuestaApi.json())
- .then(data => data.results.forEach(personaje => {
-  arrayPersonajes.push(personaje)
-}))
-.catch(console.warn)
+let nombreDePel = ''
+let generoDePel = ''
+let descDePel = ''
 
-const obtenerPersonaje = () => {
-  console.log(arrayPersonajes)
-  divPadre.innerHTML = arrayPersonajes.map((personaje) => `
-  <div class=" container-fluid">
-      <div class="row ">
-          <div class="card col-md-2 col-lg-9 pt-3 " style="width: 13rem" ;>
-          <img src="${personaje.image}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Titulo/h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Más información</a>
-          </div>
-        </div> 
-          </div>
-      </div>
+const createProd = (evento) => {
+  const { name, value } = evento.target
+
+  if (name === 'nombrePelicula') {
+    nombreDePel = value
+  } else if (name === 'genero') {
+    generoDePel = value
+  } else if (name === 'desc') {
+    descDePel = value
+  }
+}
+
+nombrePelicula.addEventListener('input', createProd)
+genero.addEventListener('input', createProd)
+desc.addEventListener('input', createProd)
+
+const funcionPush = () => {
+  if (nombreDePel && generoDePel && descDePel) {
+    arrayPel.push({
+      nombrePelicula: nombreDePel,
+      genero: generoDePel,
+      desc: descDePel
+    })
+  }
+
+  if (arrayPel.length > 0) {
+    localStorage.setItem('arrayPel', JSON.stringify(arrayPel))
+    reloadProd()
+  }
+}
+//mostrar en tarjeta  lo de localstorage
+const reloadProd = () => {
+  let arrayPelicula = JSON.parse(localStorage.getItem('arrayPel'))
+  // Al div que obtuvimos por JS utilizamos la propiedad JS innerHTML y mapeamos el array y por cada elemento que tenga el array devuelve al html un elemento como por ejemplo la card. Si tenemos 4 elementos en el array devuelve 4 card 
+ divPadre.innerHTML = arrayPelicula.map((pel) =>
+    `
+  <div class="card mt-5" style="width: 18rem;">
   
+  <div class="card-body">
+    <h5 class="card-title">${pel.nombrePelicula}</h5>
+    <p class="card-text">${pel.genero}</p>
+    <p class="card-text">${pel.desc}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
-  
-  `)
-}  */
-
-// CREACIÓN DEL ARRAY: 
-const arrayPelicula = [
-  { id:1,
-    titulo: 'El elefante es un mágico elefante',
-    genero: 'Comedia',
-    descripcion: 'Película infantil',
-    img:'https://www.pluggedin.com/wp-content/uploads/2023/03/the-magicians-elephant.jpg'
-},
-{
-    id:2,
-    titulo: 'Avatar 3',
-    genero: 'Ciencia ficción',
-    descripcion: 'Película estadounidence',
-    img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4SXHPceBJ-r2h7uBkTdsSWYPELapczW0Knw&usqp=CAU'
-  },
-  {
-    id:3,
-    titulo: 'Siete reyes deben morir',
-    genero: 'Drama, ciencia ficción',
-    descripcion: 'Película española',
-    img:'https://cloudfront-us-east-1.images.arcpublishing.com/infobae/4NCWCXDSNZHJVF3F4KWBMTIA2M.jpg'
-  }
-      
-]
-
-const divPadre = document.getElementById('cardPelicula')
-
-console.log(arrayPelicula)
-divPadre.innerHTML = arrayPelicula.map((pelicula) => `
-<div class="container-fluid ">
-   <div class="row pt-2 justify-content-center">
-       <div  class=" card col-sm-12 col-md-4 col-lg-4 pt-3 " style="width: 18rem" ;>
-       <img src="${pelicula.img}" class="card-img-top " alt="...">
-       <div class="card-body text-center">
-         <h5 class="card-title">${pelicula.titulo}</h5>
-         <p class="card-text ">${pelicula.genero}</p>
-         <p class="card-text ">${pelicula.descripcion}</p>
-         <button class="btn btn-success" onclick="agregarLista(${pelicula.id})" type="button">Buscar</button>
-
-       </div>
-     </div> 
-       </div>
-   </div>
-
 </div>
-
-`).join('');
-
-const agregarLista = (id) => {
-  const peliculaFilter = arrayPelicula.filter((pelicula) => 
-    pelicula.id === id)
-  }
-
-
-
-
-
-
-
-
+`
+  )
+} 
 
 
 
