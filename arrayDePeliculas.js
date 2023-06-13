3d74a85548cb336d51c5f76b34707aacfb2bae61
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2023-06-12 21:28:08 -0300

    se agrega d-flex en el div padre de vistaUsuarioLogueado.html y se crea localstorage en arrayDePeliculas.js

--- arrayDePeliculas.js
+++ arrayDePeliculas.js
@@ -1,34 +1,3 @@
-/*  const arrayPersonajes = []
-
- const divPadre = document.getElementById('cardPelicula')
-
- const personajes = fetch('https://rickandmortyapi.com/api/character')
- .then(repuestaApi => repuestaApi.json())
- .then(data => data.results.forEach(personaje => {
-  arrayPersonajes.push(personaje)
-}))
-.catch(console.warn)
-
-const obtenerPersonaje = () => {
-  console.log(arrayPersonajes)
-  divPadre.innerHTML = arrayPersonajes.map((personaje) => `
-  <div class=" container-fluid">
-      <div class="row ">
-          <div class="card col-md-2 col-lg-9 pt-3 " style="width: 13rem" ;>
-          <img src="${personaje.image}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Titulo/h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Más información</a>
-          </div>
-        </div> 
-          </div>
-      </div>
-  
-  </div>
-  
-  `)
-}  */
 
 // CREACIÓN DEL ARRAY: 
 const arrayPelicula = [
@@ -55,6 +24,9 @@ const arrayPelicula = [
         
 ]
 
+//se crea localstorage
+localStorage.setItem('peliculas',JSON.stringify(arrayPelicula))
+//.......
 const divPadre = document.getElementById('cardPelicula')
 
  console.log(arrayPelicula)
