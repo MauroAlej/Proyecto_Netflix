d055b6a464ff037243ef254f7f448162f7b6d6fc
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2023-06-13 18:43:36 -0300

    se realizan modificaciones en las cards, se las coloca en el centro del contenedor y elimina botones de cada una en vistaUsuarioLogueado

--- arrayDePeliculas.js
+++ arrayDePeliculas.js
@@ -31,23 +31,16 @@ const divPadre = document.getElementById('cardPelicula')
 
  console.log(arrayPelicula)
  divPadre.innerHTML = arrayPelicula.map((pelicula) => `
- <div class="container-fluid ">
-     <div class="row pt-2 justify-content-center">
-         <div  class=" card col-sm-12 col-md-4 col-lg-4 pt-3 " style="width: 18rem" ;>
+ 
+         <div  class=" card  " style="width: 18rem" ;>
          <img src="${pelicula.img}" class="card-img-top " alt="...">
          <div class="card-body text-center">
            <h5 class="card-title">${pelicula.titulo}</h5>
            <p class="card-text ">${pelicula.genero}</p>
            <p class="card-text ">${pelicula.descripcion}</p>
-           <button class="btn btn-success" onclick="agregarLista(${pelicula.id})" type="button">Buscar</button>
-
-         </div>
+        </div>
        </div> 
-         </div>
-     </div>
- 
- </div> 
- 
+
  `).join('');
 
  const agregarLista = (id) => {
