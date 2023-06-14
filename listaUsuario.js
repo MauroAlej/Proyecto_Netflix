5c4baed53392a0c6247d920ba617d67aa388394c
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2023-06-13 23:58:40 -0300

    se cambia opción del menu por Mi lista en listaUsuario.html y vistaUsuarioLogueado.html, se muestra las cards de la lista de usuario de otra manera en listaUsuario.js, se elimina la opción de buscar en listaUsuario.html

--- listaUsuario.js
+++ listaUsuario.js
@@ -1,7 +1,7 @@
 const cardDeListaUsuario = JSON.parse(localStorage.getItem('listaUsuario'))
 const divCardsLista = document.getElementById('divCardsLista')
 
-divCardsLista.innerHTML = cardDeListaUsuario.map((pelicula) => 
+/* divCardsLista.innerHTML = cardDeListaUsuario.map((pelicula) => 
 ` 
 <div class="card mx-3 text-center " style="width: 18rem;">
   <img src="${pelicula.img}" class="card-img-top" alt="...">
@@ -15,4 +15,24 @@ divCardsLista.innerHTML = cardDeListaUsuario.map((pelicula) =>
 </div>
 `
 )
-.join('');
+.join(''); */
+
+cardDeListaUsuario.forEach(pelicula => {
+    const card = document.createElement('div')
+
+   card.innerHTML = `
+   <div class="card m-3 text-center  " style="width: 18rem;">
+  <img src="${pelicula.img}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${pelicula.titulo} </h5>
+    <p class="card-text">Género:  ${pelicula.genero}</p>
+    <p class="card-text">${pelicula.descripcion}</p>
+    <button type='button' class="btn btn-outline-success" onclick="eliminarListaUsuario(${pelicula.id})">Eliminar</button>
+
+  </div>
+</div>
+   `
+   divCardsLista.appendChild(card)
+
+});
+
