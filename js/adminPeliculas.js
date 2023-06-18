e03a83c54d444ee8020677077807e94bd55d431a
maurojuarez27851@gmail.com <maurojuarez27851@gmail.com>
2023-06-17 23:06:37 -0300

    revision de la tabla admin

--- js/adminPeliculas.js
+++ js/adminPeliculas.js
@@ -4,6 +4,8 @@ console.log(movieLocalStorage)
 
 let tbodyMovie = document.getElementById('tbodyMovie')
 tbodyMovie.innerHTML = movieLocalStorage.map ((movie)=> 
+  movie.deleted=== false
+  ?
  
  `
  <tr>
@@ -13,45 +15,74 @@ tbodyMovie.innerHTML = movieLocalStorage.map ((movie)=>
     <td> ${movie.año}</td>
     <td>     
     
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${movie.id}">
       Editar
     </button>
-    
    
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <button id= "buttonDeletedMovie" class= "btn btn-danger" onclick= 'deleteMovie(${movie.id})'> Eliminar </button>
+   
+    <div class="modal fade" id="exampleModal${movie.id}" tabindex="-1" aria-labelledby="exampleModalLabel${movie.id}" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <h1 class="modal-title fs-5 text-black" id="exampleModalLabel${movie.id}">Pelicula</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
           <form>
           <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <label for="exampleInputEmail1${movie.id}" class="form-label text-black">Nombre</label>
+            <input type="text" class="form-control" id="inputNombre${movie.id}" aria-describedby="emailHelp" value=${movie.nombre} name='nombre'>
           </div>
           <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <label for="exampleInputPassword1${movie.id}" class="form-label text-black">Género</label>
+            <input type="text" class="form-control" id="exampleInputPassword1${movie.id}">
           </div>
-          <button type="button" class="btn btn-primary">Submit</button>
+          <button type="button" class="btn btn-primary" onclick='editMovie(${movie.id})'>Guardar Cambios</button>
         </form>
           </div>
           
         </div>
       </div>
     </div>
-      <button>Eliminar</button>
+    
     </td>
  `
+ :
+ ''
 
 ).join('')
 
-const editarPeliculas = document.getElementById('editPelicula')
+const inputNombre =document.querySelectorAll ('input[name="nombre"]');
+const buttonDeletedMovie = document.getElementById('buttonDeletedMovie')
 
-const editMovie = () => {
+let inputChangeMovie = ''
 
+const editFunctionMovie = (event) => {
+  const { value } = event.target;
+  inputChangeMovie = value
 }
 
-editarPeliculas.addEventListener('click', editMovie)
+inputNombre.forEach((inputNombre)=> {
+  inputNombre.addEventListener('input', editFunctionMovie);
+});
+
+
+
+const editMovie = (id) => {
+  const movieEditIndex = movieLocalStorage.findIndex((movie) => movie.id === id)
+    movieLocalStorage[movieEditIndex].nombre = inputChangeMovie
+    localStorage.setItem('peliculas', JSON.stringify(movieLocalStorage))
+    inputChangeMovie= ''
+    console.log(movieLocalStorage)
+    location.reload()
+}
+
+const deleteMovie = (id) => {
+     
+  const movieIndex = movieLocalStorage.findIndex((movie) => movie.id === id)
+  movieLocalStorage[movieIndex].deleted = true
+  localStorage.setItem('peliculas', JSON.stringify(movieLocalStorage))
+  location.reload()
+ 
+}
