dc82d38934008f70e61c4141ebc155dd948a70ae
noelia zelaya <zelayan@gestion.com>
2023-06-21 11:44:29 -0300

    se agregan validaciones en login2.js y se cambia el color de los botones de carrito.html y compra.html, se centra sus cards y se agrega padding. Además se agrega mx en app.js para separar las cards. También se cambia el color e iconos de admin.html y admin.js

--- js/login2.js
+++ js/login2.js
@@ -2,34 +2,34 @@ let inputUser = document.getElementById('inputUser')
 let passUser = document.getElementById('inputPass')
 let buttonLogin = document.getElementById('buttonLogin')
 
-//probando
+
 let divErrUser = document.getElementById('divErrUser')
 let divErrPass = document.getElementById('divErrPass')
-//-----
+
 
 
 let localStorageArray = JSON.parse(localStorage.getItem('users')) || []
 let arrayUsuarios = []
 
-//probando
-divErrUser.classList='d-none'
-divErrPass.classList='d-none'
+
+divErrUser.classList = 'd-none'
+divErrPass.classList = 'd-none'
 
 
-//---
 
-let objetoForm= {
+
+let objetoForm = {
 
     username: '',
     pass: ''
-   
+
 }
 
-const inputChange = (event) =>{
-    const {name,value}= event.target
-    objetoForm[name]=value
- 
-    //probando:
+const inputChange = (event) => {
+    const { name, value } = event.target
+    objetoForm[name] = value
+
+
     switch (name) {
         case 'username':
             divErrUser.classList = 'd-none'
@@ -37,53 +37,54 @@ const inputChange = (event) =>{
         case 'pass':
             divErrPass.classList = 'd-none'
             break;
-        }
- //--- 
+    }
 
-   
+
+
 }
 
-const login = () =>{
+const login = () => {
 
-    
-    const {username, pass} = objetoForm
-//probando
-    if(!username && !pass){
+
+    const { username, pass } = objetoForm
+
+
+    if (!username && !pass) {
         alert('Debe completar los campos usuario y contraseña')
-    }else if(!username){
+    } else if (!username) {
         divErrUser.classList = 'd-blox text-danger'
 
-    }else if(!pass){
+    } else if (!pass) {
         divErrPass.classList = 'd-blox text-danger'
     }
-//--
+
 
     let usuario = localStorageArray.filter(usuario => {
-        return usuario.username === username 
-    }) 
+        return usuario.username === username
+    })
 
     let usuarioIndex = localStorageArray.findIndex(usuario => {
-        return usuario.username === username 
-    }) 
+        return usuario.username === username
+    })
     console.log(usuarioIndex)
 
-    if(usuario.length > 0){
-        if(usuario[0].pass === pass){
-            if(usuario[0].role === 'admin'){
+    if (usuario.length > 0) {
+        if (usuario[0].pass === pass) {
+            if (usuario[0].role === 'admin') {
                 localStorageArray[usuarioIndex].login = true
                 localStorage.setItem('users', JSON.stringify(localStorageArray))
                 location.href = 'admin.html'
-            } else{
+            } else {
                 localStorageArray[usuarioIndex].login = true
                 localStorage.setItem('users', JSON.stringify(localStorageArray))
-                
-                 location.href = 'vistaUsuarioLogueado.html'
+
+                location.href = 'vistaUsuarioLogueado.html'
             }
         }
     }
-    
+
 }
 
 inputUser.addEventListener('input', inputChange)
-passUser.addEventListener('input',inputChange)
-buttonLogin.addEventListener('click',login)
+passUser.addEventListener('input', inputChange)
+buttonLogin.addEventListener('click', login)
